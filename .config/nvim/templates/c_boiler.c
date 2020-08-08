11f8a0c60e5c81112deb0b14ed62482a3d0df41a
salman-abedin <salman104alvi@gmail.com>
2020-08-08 17:01:05 +0600

    replaced cronie with fcron

--- .config/nvim/templates/c_boiler.c
+++ .config/nvim/templates/c_boiler.c
@@ -1,9 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
-#include <unistd.h>
 
-int main(int argc, char *argv[]) {
-  printf("Hello World");
-  return 0;
+int main(int argc, char* argv[]) {
+   printf("Hello World");
+   return 0;
 }
